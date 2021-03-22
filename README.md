a9200b42a779bf234ca7fd9052edb6d582debe59
Your Name <basaijapascal9@gmail.com>
2021-03-22 16:30:13 +0300

    Modify readme

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # Restaurant-page
-A simple dynamic restaurant home page
+A simple restaurant home page with menu and contacts browsed using tabs
+- menu
+- contact
+
+Project is built using Js modules for easy code organization and mentainability.
+
+
+
+## Built with;
+- HTML 
+- CSS
+- JavaScript
+- Webpack
+- Linters
+
+## Getting started
+
+Clone or fork the source code 
+
+Open the folder where you cloned it into your code editor
+
+navigate to ```dist/index.html``` and open in the browser
+
+## Author
+**Pascal Byabasaija**
+- Linkedin: [Pascal Byabasaija](https://www.linkedin.com/in/pascal-byabasaija/)
+- Github: [@Byabasaija](https://github.com/Byabasaija)
+
+## 🤝 Contributions
+
+Contributions, issues and feature requests are welcome!
+
+Feel free to check the [issues page](issues/).
+
+
+## Show your support
+
+Give a ⭐️ if you like this project!
--- src/nav.js
+++ src/nav.js
@@ -5,11 +5,11 @@ import {createMenu} from './menu'
     const container = document.getElementById('content').appendChild(document.createElement('nav'))
     container.classList.add('navbar')
     const homeButton = container.appendChild(document.createElement('button'))
-    homeButton.id = home
+    homeButton.classList.add('nav-btn')
     homeButton.textContent = 'Home'   
     
     const menuButton = container.appendChild(document.createElement('button'))
-    menuButton.classList.add('menu-btn')
+    menuButton.classList.add('nav-btn')
     menuButton.textContent = 'Menu'
     menuButton.addEventListener('click', function dispmenu() {
       createMenu()
@@ -17,7 +17,7 @@ import {createMenu} from './menu'
     
    
     const contactButton = container.appendChild(document.createElement('button'))
-    contactButton.classList.add('contact-btn')
+    contactButton.classList.add('nav-btn')
     contactButton.textContent = 'Contact'
     contactButton.addEventListener('click', function dispcontact(){
       createContact()
